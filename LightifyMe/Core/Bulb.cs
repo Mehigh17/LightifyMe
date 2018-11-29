a9a820f28c167eb4960ae24d6e42d25d7074243c
STAN Mihai <kazepworld@gmail.com>
2018-11-29 23:05:53 +0100

    Add the bulb entity

---
+++ LightifyMe/Core/Bulb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace LightifyMe.Core
+{
+    public class Bulb
+    {
+
+        public ushort Id { get; set; }
+        public ulong MacAddress { get; set; }
+        public byte Type { get; set; }
+        public uint FirmwareVersion { get; set; }
+        public bool IsAvailable { get; set; }
+        public ushort GroupId { get; set; }
+        public bool IsOn { get; set; }
+        public byte Brightness { get; set; }
+        public short Temperature { get; set; }
+        public Color Color { get; set; }
+        public string Name { get; set; }
+
+    }
+}
