0d5b7698a5311c76e97d1621cf77384166683279
STAN Mihai <kazepworld@gmail.com>
2018-11-29 23:05:42 +0100

    Add the bulb builder

---
+++ LightifyMe/Core/Builders/BulbBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace LightifyMe.Core.Builders
+{
+    public class BulbBuilder : IBulbBuilder
+    {
+
+        private readonly List<byte> _bytes;
+
+        public BulbBuilder()
+        {
+            _bytes = new List<byte>();
+        }
+
+        public IBulbBuilder AddBytes(IEnumerable<byte> bytes)
+        {
+            _bytes.AddRange(bytes);
+            return this;
+        }
+
+        // Do not use simplified object initialization to ease debugging and exception catching
+        public Bulb Build()
+        {
+            var bulbData = _bytes.ToArray();
+            var bulb = new Bulb();
+            
+            bulb.Id = BitConverter.ToUInt16(bulbData, 0);
+            bulb.MacAddress = BitConverter.ToUInt64(bulbData, 2);
+            bulb.Type = bulbData[9];
+            bulb.FirmwareVersion = BitConverter.ToUInt32(bulbData, 10);
+            bulb.IsAvailable = BitConverter.ToBoolean(bulbData, 14);
+            bulb.GroupId = BitConverter.ToUInt16(bulbData, 15);
+            bulb.IsOn = BitConverter.ToBoolean(bulbData, 17);
+            bulb.Brightness = bulbData[18];
+            bulb.Temperature = BitConverter.ToInt16(bulbData, 19);
+            bulb.Color = Color.FromArgb(bulbData[24], bulbData[21], bulbData[22], bulbData[23]);
+            bulb.Name = Encoding.Default.GetString(bulbData, 25, bulbData.Length - 25);
+
+            _bytes.Clear();
+
+            return bulb;
+        }
+    }
+}
---
+++ LightifyMe/Core/Builders/IBulbBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LightifyMe.Core.Builders
+{
+    public interface IBulbBuilder
+    {
+
+        IBulbBuilder AddBytes(IEnumerable<byte> bytes);
+        Bulb Build();
+
+    }
+}
